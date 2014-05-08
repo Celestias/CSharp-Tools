037af192b1600cc4f9153ccbf13f3cd6eba3dbbe
Jessica <Celestias@gmail.com>
2014-05-08 13:26:59 -0700

    test

---
+++ WoW_Web_API_Tool/WoW_Web_API_Tool/Form1.Designer.cs
@@ -0,0 +1,190 @@
+﻿namespace WoW_Web_API_Tool
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.GETBtn = new System.Windows.Forms.Button();
+            this.EnterIdTb = new System.Windows.Forms.TextBox();
+            this.GetControllerUD = new System.Windows.Forms.NumericUpDown();
+            this.ApiSelectCbo = new System.Windows.Forms.ComboBox();
+            this.userLbl1 = new System.Windows.Forms.Label();
+            this.RndIdCb = new System.Windows.Forms.CheckBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.ResultRptTb = new System.Windows.Forms.TextBox();
+            this.ResetRptBtn = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.GetControllerUD)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // GETBtn
+            // 
+            this.GETBtn.Location = new System.Drawing.Point(13, 152);
+            this.GETBtn.Name = "GETBtn";
+            this.GETBtn.Size = new System.Drawing.Size(101, 23);
+            this.GETBtn.TabIndex = 4;
+            this.GETBtn.Text = "Send GET";
+            this.GETBtn.UseVisualStyleBackColor = true;
+            this.GETBtn.Click += new System.EventHandler(this.GetRequest_Click);
+            // 
+            // EnterIdTb
+            // 
+            this.EnterIdTb.Location = new System.Drawing.Point(13, 75);
+            this.EnterIdTb.MaxLength = 99999;
+            this.EnterIdTb.Name = "EnterIdTb";
+            this.EnterIdTb.Size = new System.Drawing.Size(120, 20);
+            this.EnterIdTb.TabIndex = 2;
+            // 
+            // GetControllerUD
+            // 
+            this.GetControllerUD.Location = new System.Drawing.Point(14, 126);
+            this.GetControllerUD.Maximum = new decimal(new int[] {
+            9999,
+            0,
+            0,
+            0});
+            this.GetControllerUD.Name = "GetControllerUD";
+            this.GetControllerUD.Size = new System.Drawing.Size(120, 20);
+            this.GetControllerUD.TabIndex = 3;
+            this.GetControllerUD.ValueChanged += new System.EventHandler(this.GetControllerUD_ValueChanged);
+            // 
+            // ApiSelectCbo
+            // 
+            this.ApiSelectCbo.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.ApiSelectCbo.FormattingEnabled = true;
+            this.ApiSelectCbo.Items.AddRange(new object[] {
+            "Item",
+            "Item Set"});
+            this.ApiSelectCbo.Location = new System.Drawing.Point(12, 28);
+            this.ApiSelectCbo.Name = "ApiSelectCbo";
+            this.ApiSelectCbo.Size = new System.Drawing.Size(121, 21);
+            this.ApiSelectCbo.TabIndex = 1;
+            // 
+            // userLbl1
+            // 
+            this.userLbl1.AutoSize = true;
+            this.userLbl1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.userLbl1.Location = new System.Drawing.Point(15, 56);
+            this.userLbl1.Name = "userLbl1";
+            this.userLbl1.Size = new System.Drawing.Size(99, 13);
+            this.userLbl1.TabIndex = 5;
+            this.userLbl1.Text = "Enter an Item Id | or";
+            // 
+            // RndIdCb
+            // 
+            this.RndIdCb.AutoSize = true;
+            this.RndIdCb.Location = new System.Drawing.Point(141, 55);
+            this.RndIdCb.Name = "RndIdCb";
+            this.RndIdCb.Size = new System.Drawing.Size(91, 17);
+            this.RndIdCb.TabIndex = 6;
+            this.RndIdCb.Text = "Randomize Id";
+            this.RndIdCb.UseVisualStyleBackColor = true;
+            this.RndIdCb.CheckedChanged += new System.EventHandler(this.RndIdCb_CheckedChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(13, 102);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(160, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Specify number of GET requests";
+            // 
+            // ResultRptTb
+            // 
+            this.ResultRptTb.Location = new System.Drawing.Point(10, 189);
+            this.ResultRptTb.Multiline = true;
+            this.ResultRptTb.Name = "ResultRptTb";
+            this.ResultRptTb.ReadOnly = true;
+            this.ResultRptTb.Size = new System.Drawing.Size(212, 88);
+            this.ResultRptTb.TabIndex = 7;
+            this.ResultRptTb.Text = "Results:";
+            // 
+            // ResetRptBtn
+            // 
+            this.ResetRptBtn.Location = new System.Drawing.Point(122, 152);
+            this.ResetRptBtn.Name = "ResetRptBtn";
+            this.ResetRptBtn.Size = new System.Drawing.Size(102, 23);
+            this.ResetRptBtn.TabIndex = 5;
+            this.ResetRptBtn.Text = "Reset Report";
+            this.ResetRptBtn.UseVisualStyleBackColor = true;
+            this.ResetRptBtn.Click += new System.EventHandler(this.ResetRptBtn_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(156, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Select a WoW API from the list:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(276, 289);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.ResetRptBtn);
+            this.Controls.Add(this.ResultRptTb);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.RndIdCb);
+            this.Controls.Add(this.userLbl1);
+            this.Controls.Add(this.ApiSelectCbo);
+            this.Controls.Add(this.GetControllerUD);
+            this.Controls.Add(this.EnterIdTb);
+            this.Controls.Add(this.GETBtn);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "WoW API Web Request Tool";
+            ((System.ComponentModel.ISupportInitialize)(this.GetControllerUD)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button GETBtn;
+        private System.Windows.Forms.TextBox EnterIdTb;
+        private System.Windows.Forms.NumericUpDown GetControllerUD;
+        private System.Windows.Forms.ComboBox ApiSelectCbo;
+        private System.Windows.Forms.Label userLbl1;
+        private System.Windows.Forms.CheckBox RndIdCb;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox ResultRptTb;
+        private System.Windows.Forms.Button ResetRptBtn;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ WoW_Web_API_Tool/WoW_Web_API_Tool/Form1.cs
@@ -0,0 +1,267 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Net;
+using System.IO;
+
+namespace WoW_Web_API_Tool
+{
+    /****************************************************************
+     * Version 1.0 - WoW Web API Tool-  Written by Jessica Rizzo 4-27-2014
+     *
+     * This tool allows a user to test calls to the Item and Item Set 
+     * API's which are part of the WoW Web API system.  
+     * 
+     * The tool makes HTTP Requests and receives HTTP Responses and tracks
+     * the results.  A user can specify if they wish to call the Item API
+     * or if they wish to call the Item Set API.  The user can specify
+     * an item Id they wish to send in their request.  In addition they can
+     * specify how many times they wish to send the request.
+     * 
+     * Additional Features:
+     * -Added a randomize Item Id function which will send random Ids to
+     * test for broader coverage across each API.
+     * -Added reporting which will record how many successes or failures
+     *   -Tracking for failures caused by timeout
+     *   -Tracking for failures caused by item not found.
+     * 
+     * Updated - 4-27-14
+     * -Added Error log structure to save a file with the contents of
+     *  the exceptions. (note this has not been fully implemented as of 4-27-14)
+     *     
+     *
+     * * *************************************************************/
+
+
+
+    public partial class Form1 : Form
+    {
+        string itemId;
+        string urlAddress;
+        string getUrlAddress;
+        string Host;
+        string GetData;
+        byte[] data;
+
+        // Reporting Global Variables
+        int timeOutCounter = 0;
+        int reportError = 0;
+        int numberOfGetsSent = 0;
+        int getSucceeded = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public string SetUrl
+        {
+            set
+            {
+                // If the item API is selected and the Randomize checkbox is NOT selected then use the Item ID specified by the user.
+                if (ApiSelectCbo.Text == "Item" && (!(RndIdCb.Checked)))
+                {
+                    itemId = EnterIdTb.Text;      
+                    Host = "http://us.battle.net";
+                    urlAddress = Host + "/api/wow/item/" + itemId;
+                    GetData = urlAddress + EnterIdTb.Text;
+                    data = Encoding.ASCII.GetBytes(GetData);
+                }
+                
+                // If the Item API is selected and the Randomize checkbox is also selected then randomize an item ID for the user.
+                else if (ApiSelectCbo.Text == "Item" && (RndIdCb.Checked))
+                {
+                    Random random = new Random();
+                    int randItemId = random.Next(0, 99999);
+                    Host = "http://us.battle.net";
+                    urlAddress = Host + "/api/wow/item/" + randItemId;
+                    GetData = urlAddress + EnterIdTb.Text;
+                    data = Encoding.ASCII.GetBytes(GetData);
+                }
+                else if (ApiSelectCbo.Text == "Item Set" && (RndIdCb.Checked))
+                {
+                    Random random = new Random();
+                    int randSetItemId = random.Next(0, 1300);
+                    Host = "http://us.battle.net";
+                    urlAddress = Host + "/api/wow/item/set/" + randSetItemId;
+                    GetData = urlAddress + EnterIdTb.Text;
+                    data = Encoding.ASCII.GetBytes(GetData);
+                }
+                else
+                {
+                    itemId = EnterIdTb.Text;
+                    Host = "http://us.battle.net";
+                    urlAddress = Host + "/api/wow/item/set/" + itemId;
+                    GetData = urlAddress + EnterIdTb.Text;
+                    data = Encoding.ASCII.GetBytes(GetData);
+                }
+            }
+        }
+
+        // The SetGet property ensures that the Get KeyUpDown control value is always greater than 0.
+
+        public int SetGet
+        {
+            set
+            {
+                if (!(GetControllerUD.Value > 0))
+                {
+                    MessageBox.Show("The number of GET requests must be more than 0.");
+                    GetControllerUD.Focus();
+                }
+            }
+        }
+
+        private void GetRequest_Click(object sender, EventArgs e)
+        {
+            if (ApiSelectCbo.Text == "")
+            {
+                MessageBox.Show("You must first select an API");
+                ApiSelectCbo.Focus();
+            }
+
+            else if (!(GetControllerUD.Value > 0))
+            {
+                MessageBox.Show("The number of GET requests must be more than 0.");
+                GetControllerUD.Focus();
+            }
+            else
+                // We will continue the loop to send Get Requests to the server until our counter variable is no longer equivalent to the
+                // user specified KeyUpDown value. 
+                // Using a private numberOfGetsSent variable we will keep track of how many times a request has been sent for reporting purposes.
+                for (int i = 0; i < GetControllerUD.Value; i++)
+                {
+                    GetRequest();
+                    numberOfGetsSent++;
+                }
+            UpdateReport();
+        }
+
+        private void GetRequest()
+        {
+
+            try
+            {
+                HttpRequestData();
+            }
+
+            // We catch the exceptions, sectioning out timeout exceptions from general exceptions.
+
+            catch (Exception ex)
+            {   
+                if (ex is WebException && ex.Message.Contains("The operation has timed out"))
+                {
+                    timeOutCounter++;
+                //  MessageBox.Show("A Timeout has occurred with the GET request. " + "\n" + "Current Count: " + timeOutCounter);
+                }
+
+                // *Generally other exceptions are caused by a 404 returned by the HTTP request -- This is typically indicative of Status | reason "nok" 
+                else
+                {
+                //  MessageBox.Show("The following error occurred: " + ex.Message);
+                    reportError++;
+                }                
+            } 
+        }
+
+        // This method controls the entierty of the HTTP Request, HTTP Response and Streamreader information.  
+        // It handles creating the request, reading and closing the stream.
+
+        public void HttpRequestData()
+        {
+            SetUrl = "";
+            getUrlAddress = urlAddress;
+            HttpWebRequest req = (HttpWebRequest)WebRequest.Create(getUrlAddress);
+            req.Method = "GET";
+            req.ContentType = "application/json";
+
+            // Sets the timeout value for testing purposes
+            req.Timeout = 1;
+            HttpWebResponse resp = (HttpWebResponse)req.GetResponse();
+
+            string GetData = getUrlAddress;
+            byte[] data = Encoding.ASCII.GetBytes(GetData);
+            Stream stream = resp.GetResponseStream();
+            stream.Read(data, 0, data.Length);
+            StreamReader reader = new StreamReader(stream);
+            reader.ReadToEnd();
+            reader.Close();
+            stream.Close();
+            getSucceeded++;
+        }
+
+        // Any time the Get KeyUpDown controls value changes we check it's property condition.
+
+        private void GetControllerUD_ValueChanged(object sender, EventArgs e)
+        {
+            SetGet = (int)GetControllerUD.Value;
+        }
+
+        // If this checkbox is enabled the control that handles sending individual Item or Item Sets will be disabled and cleared
+        // When calling GetRequest() the system will check the status of this checkbox to determine if the Item/Set Id should be randomized or not.
+        private void RndIdCb_CheckedChanged(object sender, EventArgs e)
+        {
+            if (RndIdCb.Checked)
+            {
+                EnterIdTb.Text = "";
+                EnterIdTb.Enabled = false;
+                
+            }
+            else 
+                EnterIdTb.Enabled = true;
+        }
+
+        // The reporting function keeps track of all of the reporting variables and updates it on the form.
+        // This will list out the number of requests sent, the number of successes, failures and the likely types of failure (timeout, etc.) for the user.
+
+        private void UpdateReport()
+        {
+            if (numberOfGetsSent > 0)
+            {
+                ResultRptTb.Text = "GET Request Results for " + numberOfGetsSent + " requests:" + "\r\n" + "\r\n" +
+                                    "Succeeded: " + getSucceeded +
+                                    "\r\n" + "\r\n" + "Failed: " + (timeOutCounter + reportError) + "\r\n" + "Timed out: "
+                                    + (int)timeOutCounter + "\t" + "Item Not Found: " + reportError;
+                if (timeOutCounter > 0)
+                {
+                    DialogResult resultmsg = MessageBox.Show("Server timeouts were detected in the response" + "\r\n" + "Do you wish to Save a Log?",
+                                                            "Warning", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+
+                    // TODO: This section is reserved for future versions -- We will be saving an Error log which the user will save
+                    // 4-27-14 -- Added SaveDialogue and streamwriter Open file logic.
+                    {
+                        if (resultmsg == DialogResult.Yes)
+                        {
+                            SaveFileDialog save = new SaveFileDialog();
+
+                            DateTime currentDateTime = DateTime.Now;
+                            //currentDateTime.ToString("dd-MM-yyy-hh-mm-ss");
+                            save.FileName = "WoWAPIErrorLog" + currentDateTime.ToString("MM-dd-HH-mm-ss");
+                            save.Filter = "Text File | *.txt";
+                            if (save.ShowDialog() == DialogResult.OK)
+                            {
+                                StreamWriter sw = new StreamWriter(save.OpenFile());
+
+                            }
+                        }
+                    }
+                }
+            }
+
+        }
+
+        // This method resets the reporting display on the form and sets the global variables back to their defaults
+        private void ResetRptBtn_Click(object sender, EventArgs e)
+        {
+            ResultRptTb.Text = "Results:";
+            timeOutCounter = 0;
+            reportError = 0;
+            numberOfGetsSent = 0;
+            getSucceeded = 0;
+        }
+    }
+}
